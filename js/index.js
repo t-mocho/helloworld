355851f140edd46ba6870d13fefd7396ce998209
minao@genba <taniyasonn@gmail.com>
2020-02-05 18:02:43 +0900

    全体フェードイン＆アウト

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// 1文字ずつ表示
+
+// スタート画面
+$(function() {
+  setTimeout(function() {
+    $('#start p').fadeIn(1000);
+  }, 500);
+  setTimeout(function() {
+    $('#start').fadeOut(1000);
+  }, 2500);
+});
