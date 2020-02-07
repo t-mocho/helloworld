0f5d2bbd97f9a24fa21d7f56564f0c04176021e2
t-mocho <55965167+t-mocho@users.noreply.github.com>
2020-02-07 14:01:37 +0900

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
+$(function() {
+  function startAnime() {
+    startIn()
+      .then(startOut)
+      .then(menuIn);
+  }
 
+  // startInの処理
+  function startIn() {
+    var d = new $.Deferred;
+    $("#start p").fadeIn(2000, function() {
+      d.resolve();
+    });
+    return d.promise();
+  }
+
+  function startOut() {
+    var d = new $.Deferred;
+    $("#start").toggle('clip', 1500, function() {
+      d.resolve();
+    });
+    return d.promise();
+  }
+
+  function menuIn() {
+    var d = new $.Deferred;
+    $(".no-display").fadeIn(1500, function() {
+      d.resolve();
+    });
+    return d.promise();
+  }
+  // 実行
+  startAnime();
+});
